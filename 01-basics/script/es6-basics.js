b77576f2575caf8b490d8c71f96ff053192b64ff
Sumit Khandelwal <sumitkhandelwal@Sumits-MacBook-Air.local>
2023-07-24 15:54:59 +0530

    map and set

--- 01-basics/script/es6-basics.js
+++ 01-basics/script/es6-basics.js
@@ -71,21 +71,135 @@
 
 // - Spread expands the collection into individual elements
 
-let numbers = [3, 4, 5];
+// let numbers = [3, 4, 5];
 
-let moreNumbers = [1, 2, ...numbers, 6, 7, 8];
+// let moreNumbers = [1, 2, ...numbers, 6, 7, 8];
 
-console.log(moreNumbers);
+// console.log(moreNumbers);
 
-let userOne = {
-  name: "John",
-  isAdmin: true,
-  company: "DB",
-};
+// let userOne = {
+//   name: "John",
+//   isAdmin: true,
+//   company: "DB",
+// };
 
-let userTwo = {
-  ...userOne,
-  name: "Jeeny",
-};
+// let userTwo = {
+//   ...userOne,
+//   name: "Jeeny",
+// };
 
-console.log(userTwo);
+// console.log(userTwo);
+
+// Template Literals - ` BACK TICK `
+// - Embed the variables into string without ( + )
+// - Multi-line string without (\n)
+
+// let username = "Jenny Doe";
+// let userAge = 32;
+
+// let greet = `
+// Hello from ${username}!!
+
+// I'm ${userAge} years old..
+
+// `;
+
+// console.log(greet);
+
+// BLOCK SCOPE : restrict the scope of avriable to the nearest block
+//  - let
+//  - const
+
+// function demo(arr) {
+//   if (arr.length > 2) {
+//     const LOAD = "LOADING IMAGES...";
+//     console.log(FLASH); // ?
+//   } else {
+//     const FLASH = "FLASHING IMAGES...";
+//   }
+// }
+
+// demo([1, 2, 3, 4]);
+
+// const username = "John Doe";     // PRIMITIVE
+
+// username = "Jenny Doe";
+
+// console.log(username);
+
+// const user = {
+//   // REFERENCE
+//   name: "John Doe",
+// };
+
+// // user = {
+// //   // Can't be done
+// //   name: "Jack",
+// // };
+
+// user.name = "Jenny Doe";
+
+// console.log(user);
+
+// let fruits = ["apple", "guava", "oranges"];
+
+// fruits.push("kiwi");
+
+// console.log(fruits);
+
+// fruits = ["apple", "guava", "kiwi"];
+
+// console.log(fruits);
+
+// Dynamic Type Casting in JavaScript
+
+// let anyVar = "Hello World";
+
+// console.log(typeof anyVar);
+
+// anyVar = 231;
+
+// console.log(typeof anyVar);
+
+// anyVar = true;
+
+// console.log(typeof anyVar);
+
+// Map - to store key/value pair
+// - key should not necesarilly be a string
+
+let map = new Map();
+
+map.set("name", "John");
+
+map.set(true, 1);
+
+map.set(201, "UID");
+
+console.log(map.size);
+
+// console.log(map.get("name"));
+
+for (let value of map.values()) {
+  console.log(value);
+}
+
+for (let key of map.keys()) {
+  console.log(key);
+}
+
+for (let [key, value] of map.entries()) {
+  console.log(`${key} -> ${value}`);
+}
+
+// Set - stores uniques values only
+
+// let set = new Set();
+
+// set.add("John");
+// set.add("Jenny");
+// set.add("Jack");
+// set.add("Jill");
+// set.add("Jack");
+
+// console.log(set.size);
