c5827cfbb0d27456d3b018821c5f20aae2af0df1
Sumit Khandelwal <sumitkhandelwal@Sumits-MacBook-Air.local>
2023-07-24 15:24:55 +0530

    rest and spread

--- 01-basics/script/es6-basics.js
+++ 01-basics/script/es6-basics.js
@@ -27,32 +27,65 @@
 // console.log(f3); // Grapes
 
 // NESTED DESTRUCTURING
-let user = {
-  name: "John Doe",
-  age: 32,
-  address: {
-    street: "201 Main Road, Wakad",
-    city: "Pune",
-  },
-  friends: ["Joe", "Chandler", "Rachel"],
+// let user = {
+//   name: "John Doe",
+//   age: 32,
+//   address: {
+//     street: "201 Main Road, Wakad",
+//     city: "Pune",
+//   },
+//   friends: ["Joe", "Chandler", "Rachel"],
+// };
+
+// let {
+//   name,
+//   age,
+//   address: { city, street },
+//   friends: [f1, f2, f3],
+// } = user;
+
+// console.log(city, f3, name);
+
+// let users = [
+//   { email: "john@test", age: 32 },
+//   { email: "jenny@test", age: 30 },
+//   { email: "jack@test", age: 34 },
+// ];
+
+// let [{ email: e1, age: a1 }, { email: e2, age: a2 }, { email: e3, age: a3 }] =
+//   users;
+
+// console.log(e1, a2, e3);
+
+// REST / SPREAD (...)
+// - Rest create the collection from individual elements
+// - Supplied as last arguments in function declaration
+
+// function demo(userOneEmail, ...args) {
+//   console.log(args[0]);
+// }
+
+// demo("test@test.com");
+// demo("test@test.com", "john@test");
+// demo("test@test.com", "john@test", "jenny@test");
+
+// - Spread expands the collection into individual elements
+
+let numbers = [3, 4, 5];
+
+let moreNumbers = [1, 2, ...numbers, 6, 7, 8];
+
+console.log(moreNumbers);
+
+let userOne = {
+  name: "John",
+  isAdmin: true,
+  company: "DB",
 };
 
-let {
-  name,
-  age,
-  address: { city, street },
-  friends: [f1, f2, f3],
-} = user;
+let userTwo = {
+  ...userOne,
+  name: "Jeeny",
+};
 
-console.log(city, f3, name);
-
-let users = [
-  { email: "john@test", age: 32 },
-  { email: "jenny@test", age: 30 },
-  { email: "jack@test", age: 34 },
-];
-
-let [{ email: e1, age: a1 }, { email: e2, age: a2 }, { email: e3, age: a3 }] =
-  users;
-
-console.log(e1, a2, e3);
+console.log(userTwo);
